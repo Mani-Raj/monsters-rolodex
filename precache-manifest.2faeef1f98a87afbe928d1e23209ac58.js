296c933f668fc347da9c51dfe5d41c61935905d9
maniraj <maniraj@yahoo.com>
2020-07-18 13:35:02 +0530

    Updates

---
+++ precache-manifest.2faeef1f98a87afbe928d1e23209ac58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c8c96219117d8db039f19188dedcbc2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "45e81309c887f5d30391",
+    "url": "/monsters-rolodex/static/css/main.f0c2fa75.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45e81309c887f5d30391",
+    "url": "/monsters-rolodex/static/js/main.8df2e9b6.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
